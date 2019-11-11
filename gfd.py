3572e020e2217685310a7dff452c5cc3bc63dffb
Kevin Song <song1kevin@gmail.com>
2019-11-10 19:54:53 -0500

    code now works for timeout and receiving data/heartbeats.  Need to implement how to re-establish connection once lost

--- gfd.py
+++ gfd.py
@@ -5,6 +5,8 @@ import csv
 import timeit
 import pickle
 import select
+import datetime
+import time
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,16 +21,20 @@ def start1():
 
 #get start signals from all lfds
 def start2():
+	ip_addr = '128.237.125.54'
 	num_replicas = 1
 	global server
+	print("in start2")
 	#allow socket to be seen to outside world
-	server.bind((socket.gethostbyname(socket.gethostname()), 5005))
+	server.bind((ip_addr, 5005))
 	server.listen(1)
 	replica_ips = []
 	ports = []
 	lfd_status = []
 	replica_status = []
 	while len(replica_ips) < num_replicas:
+		print(socket.gethostbyname(socket.gethostname()))
+		print("waiting for connection from lfd")
 		conn, addr = server.accept()
 		print('Connection address:' + str(addr))
 		data = conn.recv(1024)
@@ -59,36 +65,50 @@ def start2():
 
 #get heartbeats and data from lfds
 def main():
+	print("main")
 	global server
 	start1()
 	ips, ports, conn = start2()
 	timeout = 2.0
 	while 1:
-		server.setblocking(0)
-		ready = select.select([server], [], [], timeout)
-		if ready[0]:
+		print("waiting for data in main")
+		#server.setblocking(0)
+		#ready = select.select([server], [], [], timeout)
+		data = conn.recv(1024)
+		print("recv did not hang")
+		#sock.settimeout(None)
+		if(data):
+			time = datetime.datetime.now().time()
+			time_min = time.minute
+			time_sec = time.second
+			curr_time = time_min*60+time_sec
 			print("Heartbeat Received")
-			data = conn.recv(1024)
-			try:
-				data = pickle.loads(data)
+			data = pickle.loads(data)
+			print(data)
 
-				#might want to only do this if data is unique
-				with open('gfd_port.csv', 'w') as writeFile:
-					print("writing")
-					writer = csv.writer(writeFile)
-					writer.writerow(ips)
+			#might want to only do this if data is unique
+			with open('gfd_port.csv', 'w') as writeFile:
+				print("writing")
+				writer = csv.writer(writeFile)
+				writer.writerow(ips)
+				writer.writerow("1")
+				writer.writerow(ips)
+				#need to account for num_replicas here
+				if(data["replica_status"] == "True"):
 					writer.writerow("1")
-					writer.writerow(ips)
-					#need to account for num_replicas here
-					if(data["replica_status"] == "True"):
-						writer.writerow("1")
-					else:
-						writer.writerow("0")
-					writer.writerow(ports)
-			except:
-				pass
+				else:
+					writer.writerow("0")
+				writer.writerow(ports)
 		else:
-			print("Connection Timeout")
+			time2 = datetime.datetime.now().time()
+			time_min2 = time2.minute
+			time_sec2 = time2.second
+			wait_time = time_min2*60+time_sec2
+			if(wait_time - curr_time > timeout):
+				print("connection timeout")
+				break
+
+
 
 
 
